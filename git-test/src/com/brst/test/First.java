f548054659f62646723b3ff80122e98903c4a229
LvBaiRui <2817390988@qq.com>
2020-07-28 19:28:33 +0800

    添加工程到本地git

---
+++ git-test/src/com/brst/test/First.java
@@ -0,0 +1,12 @@
+package com.brst.test;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+
+
+    }
+}
